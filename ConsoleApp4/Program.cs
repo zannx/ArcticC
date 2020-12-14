49179aa618cdd42bfbe5a52c1335e88d7fde37d7
Žan Luka Artič <zanluka.artic@gmail.com>
2020-12-14 16:18:54 +0100

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using static ArticCSharp.StringCompiler.CompilerStr;
+
+namespace ArticCSharp
+{
+
+    class Program
+    {
+   
+        static void Main(string[] args)
+        {
+
+            string SourceMain = "";
+            string Assembly = "";
+
+            while (true)
+            { 
+                string Input = Convert.ToString(Console.ReadLine());
+                
+                if (Input.ToLower() == "compile")
+                {
+                    char[] SourceMainChar = SourceMain.ToCharArray();
+
+                    string Object = "";
+                    for (int i = 0; i <= SourceMainChar.Length - 1; i++)
+                    {
+                    
+                        if (SourceMainChar[i] == 0x20)
+                        {
+                            
+                            //End
+                            if (CompilerString(Object.ToLower()) == "656E64")
+                            {
+                                Console.WriteLine(CompilerString(Object.ToLower()));
+                            }
+
+                            //Check string what it is and convert it to C#
+                            Console.WriteLine(Object.ToString());
+
+                            Object = "";
+                            continue;
+                        }
+                        Object = Object + SourceMainChar[i].ToString();
+                    }
+
+                    //Run C# code and return result
+
+                }
+                else
+                {
+                    SourceMain = Input;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/StringCompiler/CompilerStr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticCSharp.StringCompiler
+{
+    public class CompilerStr
+    {
+        public static string CompilerString(string operation)
+        {
+            char[] ArrayOperation = operation.ToCharArray();
+            string ConvertedVersion = "";
+
+            for (int i = 0; i <= operation.Length - 1; i++)
+            {
+                ConvertedVersion = ConvertedVersion + Convert.ToInt32(ArrayOperation[i]).ToString("X");
+            }
+            return ConvertedVersion;
+        }
+    }
+}
