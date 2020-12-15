7bccebe78d78ed4a512cdc09e4e0cc08cc7e5cb9
Žan Luka Artič <zanluka.artic@gmail.com>
2020-12-15 23:53:17 +0100

    Lexer small edits.

---
+++ ArcticC/Lexer/Lexer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ArcticC.StringCompiler.CompilerStr;
+using static ArcticC.StringCompiler.ByteArrays;
+using System.Linq;
+
+namespace ArcticC.Lexer
+{
+    public class Lexer
+    {
+        public static string[,] LexerCheck(char[] characterarray)
+        {
+            string[,] SourceAppart = new string[characterarray.Length, characterarray.Length];
+
+            for (int i = 0; i <= characterarray.Length - 1; i++)
+            { 
+                //Operators
+                if (operators.Contains((byte)characterarray[i]))
+                {
+                    string Together = characterarray[i].ToString();
+                    SourceAppart[1, i] = Together;
+                    SourceAppart[0, i] = "operator";
+                    if (operators.Contains((byte)characterarray[i + 1]))
+                    {
+                        Together = Together + characterarray[i + 1].ToString();
+                        SourceAppart[0, i] = "operator";
+                        SourceAppart[1, i] = Together;
+                        i++;
+                    }
+                }
+
+                //Integer or float
+                if (integers.Contains((byte)characterarray[i]))
+                {
+                    string Together = characterarray[i].ToString();
+                    SourceAppart[0, i] = "integer";
+
+                    int Count = 1;
+                    while (operators.Contains((byte)characterarray[i + Count]) || dot.Contains((byte)characterarray[i + Count]))
+                    {
+                        if (dot.Contains((byte)characterarray[i + Count]))
+                        {
+                            Together = Together + characterarray[i + 1].ToString();
+                            SourceAppart[1, i] = Together;
+                            SourceAppart[0, i] = "float";
+                            i++;
+                            continue;
+                        }
+                        Together = Together + characterarray[i + 1].ToString();
+                        SourceAppart[1, i] = Together;
+                        Count = Count + 1;
+                        i++;
+                    }
+                }
+
+                //Nothing
+                if (operators.Contains((byte)characterarray[i]))
+                {
+                    string Together = characterarray[i].ToString();
+                    SourceAppart[0, i] = Together;
+                }
+            }
+            return SourceAppart;
+        }
+    }
+}
--- ArcticC/Program.cs
+++ ArcticC/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using static ArcticC.StringCompiler.CompilerStr;
 using static ArcticC.StringCompiler.ByteArrays;
+using static ArcticC.Lexer.Lexer;
 
 namespace ArcticC
 {
 
     class Program
     {
-   
         static void Main(string[] args)
         {
+            Console.Title = "ArcticC";
+
             //Basic variables
             string SourceMain = "";
             string Assembly = "";
@@ -17,33 +19,40 @@ namespace ArcticC
             while (true)
             { 
                 string Input = Convert.ToString(Console.ReadLine());
-                
+                SourceMain = SourceMain + Convert.ToString('\x20');
+
                 if (CheckBytes(CompilerString(Input.ToLower()), compile))
                 {
                     char[] SourceMainChar = SourceMain.ToCharArray();
+                    //string[,] SourceAppart = new string[SourceMainChar.Length,SourceMainChar.Length];
 
-                    string Object = "";
-                    for (int i = 0; i <= SourceMainChar.Length - 1; i++)
-                    {
-                    
-                        if (SourceMainChar[i] == 0x20)
-                        {
-                            
-                            //End
-                            if (CheckBytes(CompilerString(Object.ToLower()), end))
-                            {
-                                Console.WriteLine(Object.ToLower());
-                            }
+                    //int Count = 0;
+                    //string Object = "";
+                    //for (int i = 0; i <= SourceMainChar.Length - 1; i++)
+                    //{
 
-                            //Check string what it is and convert it to C#
-                            Console.WriteLine(Object.ToString());
+                    //    if (SourceMainChar[i] == 0x20)
+                    //    {
 
-                            Object = "";
-                            continue;
-                        }
-                        Object = Object + SourceMainChar[i].ToString();
-                    }
+                    //        ////End
+                    //        //if (CheckBytes(CompilerString(Object.ToLower()), end))
+                    //        //{
+                    //        //    Console.WriteLine(Object.ToLower());
+                    //        //}
 
+                    //        SourceAppart[1, Count] = Object; 
+                    //        Count = Count + 1;
+
+                    //        //Check string what it is and convert it to C#
+                    //        Console.WriteLine(Object.ToString());
+
+                    //        Object = "";
+                    //        continue;
+                    //    }
+                    //    Object = Object + SourceMainChar[i].ToString();
+                    //}
+
+                    LexerCheck(SourceMainChar);
                     //Run C# code and return result
 
                 }
